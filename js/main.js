9bce1961c6e3f4baceb7b5ba319603a47af31d7f
Victor Faria <vitilevi@hotmail.com>
2022-04-17 22:24:36 -0300

    feat: add animate logic to game

---
+++ js/main.js
@@ -0,0 +1,25 @@
+import Game from './game';
+
+const game = new Game();
+
+const gameEventListener = (e) => {
+  const keyPressed = e.key;
+
+  const selectDirection = {
+    ArrowRight: () => game.animate('right'),
+    ArrowLeft: () => game.animate('left'),
+    ArrowUp: () => game.animate('up'),
+    ArrowDown: () => game.animate('down'),
+  };
+
+  const selectedAction = selectDirection[keyPressed];
+
+  if (selectedAction) {
+    selectedAction();
+  }
+};
+
+window.onload = () => {
+  game.start();
+  window.addEventListener('keydown', gameEventListener);
+};
